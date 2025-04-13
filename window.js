8c9135b294a383a46740788191b7e2439d85e30f
Arthur Simpson <a.m.simpson14@outlook.com>
2025-04-13 18:24:43 +0100

    Initial commit

---
+++ window.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var button = document.getElementById("myButt");
+    button.addEventListener("click", function() {
+        alert("You clicked my butt!");
+    });
+}
